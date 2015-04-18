6d688915447675d7a61d2226f1ab3099afb09006
kolenov <kolenov.ed@gmail.com>
2015-04-19 02:57:26 +0300

    Fix bugs

--- • Высекалка.js
+++ • Высекалка.js
@@ -1,4 +1,3 @@
-var AIversion = version.slice(0, 2);
 var doc = activeDocument;
 //  устанавливаем ноль
 doc.rulerOrigin = [0, 0];
@@ -12,12 +11,13 @@ var TextRef = doc.textFrames;
 
 // Добавляем надпись с датой и именем документа
 var My_Black = DefineCMYK(0, 0, 0, 100);
+
 The_text = My_text(10, "PragmaticaC", My_Black, "«Арт-Флекс» Днепропетровск  " + docname + " (" + TodayDate() + ")");
 The_text.top = docHeight + 30;
 The_text.left = 0;
 // ширина материала
 The_text = My_text(10, "PragmaticaC", My_Black, "ширина полотна " + Math.round(docHeight / mm) + " mm");
-The_text.position = [-textWidth / 2 - 20, docHeight / 2 + 3];
+The_text.position = [-The_text.width / 2 - 20, docHeight / 2 + 3];
 The_text.rotate(90);
 // шаг печати
 var Zub = 3.175;
@@ -26,24 +26,23 @@ var z = Math.floor(docWidth / mm / Zub);
 step = (docWidth / mm).toString();
 Width_okr = step.substr(0, 7); //отрезаем до 7 знака
 
-
 The_text = My_text(10, "PragmaticaC", My_Black, "Z=" + z + " (" + Width_okr + " mm)");
-The_text.position = [docWidth / 2 - textWidth / 2 + 50, docHeight + 60];
+The_text.position = [docWidth / 2 - The_text.width / 2 + 50, docHeight + 60];
 The_text = My_text(10, "PragmaticaC", My_Black, "--------------->");
-The_text.position = [docWidth / 2 - textWidth / 2 + 50, docHeight + 45];
+The_text.position = [docWidth / 2 - The_text.width / 2 + 50, docHeight + 45];
 
 Check = confirm("Бумага?");
 if (Check == true) {
     The_text = My_text(10, "PragmaticaC", My_Black, "Самоклеящаяся бумага Raflacoat RP51 HG65");
-    The_text.position = [docWidth / 2 - textWidth / 2 + 50, docHeight + 30];
+    The_text.position = [docWidth / 2 - The_text.width / 2 + 50, docHeight + 30];
     The_text = My_text(10, "PragmaticaC", My_Black, "Подложка: 54-57mkm");
-    The_text.position = [docWidth / 2 - textWidth / 2 + 50, docHeight + 20];
+    The_text.position = [docWidth / 2 - The_text.width / 2 + 50, docHeight + 20];
 }
 else {
     The_text = My_text(10, "PragmaticaC", My_Black, "Самоклеящаяся пленка PE6850 85mkm");
-    The_text.position = [docWidth / 2 - textWidth / 2 + 50, docHeight + 30];
+    The_text.position = [docWidth / 2 - The_text.width / 2 + 50, docHeight + 30];
     The_text = My_text(10, "PragmaticaC", My_Black, "Подложка: 54-57mkm");
-    The_text.position = [docWidth / 2 - textWidth / 2 + 50, docHeight + 20];
+    The_text.position = [docWidth / 2 - The_text.width / 2 + 50, docHeight + 20];
 }
 
 
@@ -61,17 +60,17 @@ function DefineCMYK(C, M, Y, K) {
 }
 
 function My_text(Size, Font, My_Fill, Contents) {
-        myText = TextRef.add();
-        myText.textRange.characterAttributes.size = Size;
-        myText.textRange.characterAttributes.fillColor = My_Fill;
-        try {
-            myText.textRange.characterAttributes.textFont = app.textFonts[Font];
-        }
-        catch (e) {
-            myText.textRange.characterAttributes.textFont = app.textFonts["ArialMT"];
-        }
-        myText.contents = Contents;
-        return myText;
+    myText = TextRef.add();
+    myText.textRange.characterAttributes.size = Size;
+    myText.textRange.characterAttributes.fillColor = My_Fill;
+    try {
+        myText.textRange.characterAttributes.textFont = app.textFonts[Font];
+    }
+    catch (e) {
+        myText.textRange.characterAttributes.textFont = app.textFonts["ArialMT"];
+    }
+    myText.contents = Contents;
+    return myText;
 }
 
 //  Определяем дату //
