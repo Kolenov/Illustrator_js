e01537d192e805a0bafbe99d79e8e4fc2b9d93ba
Eduard Kolenov <kolenov.ed@gmail.com>
2016-09-05 00:09:36 +0300

    Use Strict Mode. Refactoring.

--- • Шкалы.js
+++ • Шкалы.js
@@ -1,163 +1,160 @@
+"use strict";
 var doc = activeDocument;
 var docHeight = doc.height;
 var docWidth = doc.width;
 var mm = 2.834645;
-var swatchNAME = doc.swatches;
-var Start_Strip = doc.width / 2 - mm * 8; //начало первого стрипа
-var start = doc.width / 2 + mm * 4; //начало первого названия свотчеса
-var AddCMYK = "Add CMYK Swatches";
-var Shkalky = "Add Scale";
-var Name_Swatches = "Add Name Colors";
-
+var swatchNames = doc.swatches;
+var startStripPoint = doc.width / 2 - mm * 8; //начало первого стрипа
+var startSwatchNamePoint = doc.width / 2 + mm * 4; //начало первого названия свотчеса
+var addCmykMessage = "Add CMYK Swatches";
+var addScaleMessage = "Add Scales";
+var addNameSwatchesMessage = "Add Name Colors";
+var tintPercentage = [2, 10, 50, 80, 100];
 //  устанавливаем ноль
 doc.rulerOrigin = [0, 0];
 //  деселект все   //
 doc.selection = null;
 
 //------Черный цвет регистрэйшн-----//
-RegColorBL = swatchNAME[1].color;
-RegColorBL.tint = 100;
-var RegBlack = RegColorBL;
+// var regColorBlack = swatchNames[1].color;
+// regColorBlack.tint = 100;
+// var RegBlack = regColorBlack;
 //-------Белый цвет регистрэйшн-----//
-RegColorWH = swatchNAME[1].color;
-RegColorWH.tint = 0;
-var RegWhite = RegColorWH;
+var regColorWhite = swatchNames[1].color;
+regColorWhite.tint = 0;
+var regWhite = regColorWhite;
 //--------------Без цвета------------//
-NoneColorSW = swatchNAME[0].color;
-noColor = NoneColorSW;
+var noColorSwatch = swatchNames[0].color;
+var noColor = noColorSwatch;
 
+var isMakeScaleLayer = true;
+var isMakeSwatch = true;
 
-var MakeSW = 1;
-for (i = 0; i < swatchNAME.length; i++) {
-    if (swatchNAME[i].name == ["C"] || swatchNAME[i].name == ["M"] || swatchNAME[i].name == ["Y"] || swatchNAME[i].name == ["K"]) {
-        MakeSW = 0;
-    }
+for (var i = 0; i < swatchNames.length; i++) {
+	if (swatchNames[i].name == ["C"] || swatchNames[i].name == ["M"] || swatchNames[i].name == ["Y"] || swatchNames[i].name == ["K"]) {
+	   isMakeSwatch = false;
+		break;
+	}
 }
 
-if (MakeSW == 1) {
-    Check = confirm(AddCMYK);
-    if (Check == true) {
-        Spot_add("C", 100, DefineCMYK(100, 0, 0, 0)); //Cyan
-        Spot_add("M", 100, DefineCMYK(0, 100, 0, 0)); //Magenta
-        Spot_add("Y", 100, DefineCMYK(0, 0, 100, 0)); //Yellow
-        Spot_add("K", 100, DefineCMYK(0, 0, 0, 100)); //Black
-    }
+if (isMakeSwatch === true) {
+	var checkConfirm = confirm(addCmykMessage);
+	if (checkConfirm === true) {
+		addSpotColor("C", 100, DefineCMYK(100, 0, 0, 0)); //Cyan
+		addSpotColor("M", 100, DefineCMYK(0, 100, 0, 0)); //Magenta
+		addSpotColor("Y", 100, DefineCMYK(0, 0, 100, 0)); //Yellow
+		addSpotColor("K", 100, DefineCMYK(0, 0, 0, 100)); //Black
+	}
 }
 
 // ------- Шкалы ------- //
-Check = confirm(Shkalky);
-if (Check == true) {
-    MakeScaleLayer = 1;
-    Scale();
+checkConfirm = confirm(addScaleMessage);
+if (checkConfirm === true) {
 
-    for (i = 2; i < swatchNAME.length; i++) {
-        BoxGroup = doc.activeLayer.groupItems.add();
-        var NewPath = BoxGroup.pathItems;
-        Strip();
-        Start_Strip -= mm * 3;
-    }
+	makeScaleLayer();
+
+	for (i = 2; i < swatchNames.length; i++) {
+		var boxGroup = doc.activeLayer.groupItems.add();
+		var newPath = boxGroup.pathItems;
+		makeStrip();
+		startStripPoint -= mm * 3;
+	}
 
 }
 // ------- имена свотчей ------- //
-Check = confirm(Name_Swatches);
-if (Check == true) {
-    MakeScaleLayer = 1;
-    Scale();
-    for (i = 2; i < swatchNAME.length; i++) {
-        My_text();
-    }
+checkConfirm = confirm(addNameSwatchesMessage);
+if (checkConfirm === true) {
+
+	makeScaleLayer();
+
+	for (i = 2; i < swatchNames.length; i++) {
+		makeSwatchName(i);
+	}
 }
 
-function Scale() {
-    var MakeScaleLayer = 1;
-    for (j = doc.layers.length - 1; j >= 0; j--) {
-        var targetLayer = doc.layers[j];
-        var layerName = targetLayer.name;
-        if (layerName == "Scale") {
-            MakeScaleLayer = 0;
-            doc.activeLayer = doc.layers["Scale"];
-        }
-    }
-    //  Создаем новый слой для шкал  //
-    if (MakeScaleLayer == 1) {
-        var newLayer = doc.layers.add();
-        newLayer.name = "Scale"
-    }
+function makeScaleLayer() {
+	for (var i = 0 - 1; i < doc.layers.length; i++) {
+		if (doc.layers[i].name == "Scale") {
+			isMakeScaleLayer = false;
+			doc.activeLayer = doc.layers["Scale"];
+		}
+		break;
+	}
+	//  Создаем новый слой для шкал  //
+	if (isMakeScaleLayer === true) {
+		var newLayer = doc.layers.add();
+		newLayer.name = "Scale";
+		isMakeScaleLayer = false;
+	}
 }
 
-function Strip() {
-    Tint = [];
-    Tint[0] = 2;
-    Tint[1] = 10;
-    Tint[2] = 50;
-    Tint[3] = 80;
-    Tint[4] = 100;
-    for (g = 0; g < Tint.length; g++) {
-        Box();
-    }
+function makeStrip() {
+	for (var i = 0; i < tintPercentage.length; i++) {
+		makeBox(tintPercentage[i]);
+	}
 }
 
-function My_text() {
-    TextGroup = doc.activeLayer.groupItems.add();
+function makeBox(tint) {
+	var box = newPath.rectangle(mm * 3, mm * 2, mm * 3, mm * 2);
 
-    var textRef = TextGroup.textFrames.add();
+	box.stroked = true;
+	box.strokeWidth = 0.1;
+	box.filled = true;
 
-    textRef.textRange.characterAttributes.size = 8;
-    textRef.textRange.characterAttributes.stroked = false;
-    textRef.textRange.characterAttributes.wrapInside = true;
-    textRef.textRange.characterAttributes.fillColor = swatchNAME[i].color;
-    try {
-        textRef.textRange.characterAttributes.textFont = app.textFonts["PragmaticaC"];
-    } catch (e) {
-        textRef.textRange.characterAttributes.textFont = app.textFonts["ArialMT"];
-    }
-    textRef.contents = swatchNAME[i].name;
+	var boxFillColor = swatchNames[i].color;
+	boxFillColor.tint = 100;
+	box.strokeColor = boxFillColor;
+	boxFillColor.tint = tint;
+	box.fillColor = boxFillColor;
 
-
-    textWidth = textRef.width;
-    textRef.position = [start, docHeight + 1.5 - mm];
-
-    textRef.duplicate();
-
-    textRef.textRange.characterAttributes.stroked = true;
-    textRef.textRange.characterAttributes.strokeWidth = 1;
-    textRef.textRange.characterAttributes.strokeColor = RegWhite;
-
-    textRef.position = [start, docHeight + 1.5 - mm];
-    start += textWidth;
+	box.position = [startStripPoint, docHeight - mm];
+	startStripPoint -= mm * 3;
 }
 
-function Box() {
-    box = NewPath.rectangle(mm * 3, mm * 2, mm * 3, mm * 2);
+function makeSwatchName(counter) {
+	var textGroup = doc.activeLayer.groupItems.add();
 
-    box.stroked = true;
-    box.strokeWidth = 0.1;
-    box.filled = true;
+	var textRef = textGroup.textFrames.add();
 
-    FillColor = new SpotColor();
-    FillColor = swatchNAME[i].color;
-    FillColor.tint = 100;
-    box.strokeColor = FillColor;
-    FillColor.tint = Tint[g];
-    box.fillColor = FillColor;
+	textRef.textRange.characterAttributes.size = 8;
+	textRef.textRange.characterAttributes.stroked = false;
+	textRef.textRange.characterAttributes.wrapInside = true;
+	textRef.textRange.characterAttributes.fillColor = swatchNames[i].color;
+	try {
+		textRef.textRange.characterAttributes.textFont = app.textFonts["PragmaticaC"];
+	} catch (e) {
+		textRef.textRange.characterAttributes.textFont = app.textFonts["ArialMT"];
+	}
+	textRef.contents = swatchNames[counter].name;
 
-    box.position = [Start_Strip, docHeight - mm];
-    Start_Strip -= mm * 3;
+
+	var textWidth = textRef.width;
+	textRef.position = [startSwatchNamePoint, docHeight + 1.5 - mm];
+
+	textRef.duplicate();
+
+	textRef.textRange.characterAttributes.stroked = true;
+	textRef.textRange.characterAttributes.strokeWidth = 1;
+	textRef.textRange.characterAttributes.strokeColor = regWhite;
+
+	textRef.position = [startSwatchNamePoint, docHeight + 1.5 - mm];
+	startSwatchNamePoint += textWidth;
 }
 
-function Spot_add(Name, Tint, Color) {
-    var mySpots = doc.spots;
-    var newSpot = mySpots.add();
-    newSpot.color = Color;
-    newSpot.name = Name;
-    newSpot.tint = Tint;
+
+function addSpotColor(name, tint, color) {
+	var mySpots = doc.spots;
+	var newSpot = mySpots.add();
+	newSpot.color = color;
+	newSpot.name = name;
+	newSpot.tint = tint;
 }
 
 function DefineCMYK(C, M, Y, K) {
-    var newCMYKColor = new CMYKColor();
-    newCMYKColor.black = K;
-    newCMYKColor.cyan = C;
-    newCMYKColor.magenta = M;
-    newCMYKColor.yellow = Y;
-    return newCMYKColor;
+	var newCMYKColor = new CMYKColor();
+	newCMYKColor.black = K;
+	newCMYKColor.cyan = C;
+	newCMYKColor.magenta = M;
+	newCMYKColor.yellow = Y;
+	return newCMYKColor;
 }
