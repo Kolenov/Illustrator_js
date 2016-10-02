c4abc9ac30b24799b864ff68844c8fb91159930a
Eduard Kolenov <kolenov.ed@gmail.com>
2016-10-02 16:32:11 +0300

    Refactoring

--- • Рамка по документу.js
+++ • Рамка по документу.js
@@ -1,25 +1,29 @@
-﻿var doc = activeDocument;
+﻿'use strict';
+var doc = activeDocument;
 var docHeight = doc.height;
 var docWidth = doc.width;
-var pathRef;
 
-//  устанавливаем ноль   //
 doc.rulerOrigin = [0, 0];
-
-//  деселект все   //
 doc.selection = null;
 
-//  рисуем прямоугольник с отступами по 1 мм   //
-var My_Color = DefineCMYK(0, 0, 0, 100);
-pathRef = doc.activeLayer.pathItems.rectangle(docHeight, 0, docWidth, docHeight);
+// make rectangle with height = docHeight and width - docWidth
+var pathRef = doc.activeLayer.pathItems.rectangle(docHeight, 0, docWidth, docHeight);
 pathRef.filled = false;
 pathRef.stroked = true;
 pathRef.strokeWidth = 0.1;
-pathRef.strokeColor = My_Color;
+pathRef.strokeColor = defineCMYK(0, 0, 0, 100);
 pathRef.selected = true;
 
 
-function DefineCMYK(C, M, Y, K) {
+/**
+ *
+ * @param {number} C
+ * @param {number} M
+ * @param {number} Y
+ * @param {number} K
+ * @returns {CMYKColor}
+ */
+function defineCMYK(C, M, Y, K) {
     var newCMYKColor = new CMYKColor();
     newCMYKColor.black = K;
     newCMYKColor.cyan = C;
