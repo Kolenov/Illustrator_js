90a1a457c59ad3159a452bd6464ed7a56c880ea4
Eduard Kolenov <kolenov.ed@gmail.com>
2016-10-02 23:17:39 +0300

    Refactoring

--- • Таблица.js
+++ • Таблица.js
@@ -1,12 +1,15 @@
 ﻿var doc = activeDocument;
-var docHeight = doc.height;
 var docWidth = doc.width;
 var mm = 2.834645;
+var prePressTableFile = new File("c://forma.ai");
 
 if (documents.length > 0) {
     doc.rulerOrigin = [0, 0];
 
-    PrePress = new File("c://forma.ai");
-    importedGroup = doc.activeLayer.groupItems.createFromFile(PrePress);
-    importedGroup.position = [docWidth / 2 - 96 * mm, 60 * mm];
+    if (doc.activeLayer.visible && !doc.activeLayer.locked) {
+        importedGroup = doc.activeLayer.groupItems.createFromFile(prePressTableFile);
+        importedGroup.position = [docWidth / 2 - 96 * mm, 60 * mm];
+    } else {
+        alert("Set Layer " + doc.activeLayer.name + " editable");
+    }
 }
